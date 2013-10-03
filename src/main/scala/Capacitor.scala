ea9729bf7a6d2175e5c7f7932faef031bde92933
Cesar Lopez-Nataren <cesarn@mindtouch.com>
2013-10-02 18:08:19 -0700

    Add more options

--- src/main/scala/Capacitor.scala
+++ src/main/scala/Capacitor.scala
@@ -1,4 +1,5 @@
 import java.io.File
+import dispatch._, Defaults._
 
 case class Config(
     hostname: String = "",
@@ -15,24 +16,45 @@ object Capacitor {
         // Build the command line parser
         val parser = new scopt.OptionParser[Config]("capacitor") {
             head("3.x")
-            opt[String] ('h', "hostname")
-                required()
-                valueName("<hostname>")
-                action { (x, c) =>
-                    c.copy(hostname = x)
-            } text ("hostname is a string")
-            opt[String] ('u', "user")
-                required()
-                valueName("<username>")
-                action { (x, c) =>
-                    c.copy(user = x)
-            } text ("user is a string")
+            opt[String] ('h', "hostname") required() valueName("<hostname>") action { (x, c) =>
+                c.copy(hostname = x)
+            } text("hostname is a string")
+            opt[String] ('u', "user") required() valueName("<username>") action { (x, c) =>
+                c.copy(user = x)
+            } text("user is a string")
+            opt[String] ('p', "password") required() valueName("<password>") action { (x, c) =>
+                c.copy(password = x)
+            } text("password is a string")
+            opt[Int] ("numberOfPages") required() valueName("<number of pages>") action { (x, c) =>
+                c.copy(numberOfPages = x)
+            } text("numberOfPages is an integer")
+            opt[Int] ("numberOfFilesPerPage") required() valueName("<number of files per page") action { (x, c) =>
+                c.copy(numberOfFilesPerPage = x)
+            } text("numberOfFilesPerPage is an integer")
+            opt[Int] ("numberOfRevisionsPerFile") required() valueName("<number of revisions per file") action { (x, c) =>
+                c.copy(numberOfRevisionsPerFile = x)
+            }
         }
         
         // Parse
         parser.parse(args, Config()) map { config =>
             println(config.hostname)
             println(config.user)
+            println(config.numberOfPages)
+            println(config.numberOfFilesPerPage)
+            println(config.numberOfRevisionsPerFile)
         }
+        
+        // Establish connection
+        
+        // Create root page
+        
+        // Create the other pages
+        (1 .. config.numberOfPages).par.map()
+        
+        // Create the files
+        
+        // Create the files' revisions
+        
     }
 }
